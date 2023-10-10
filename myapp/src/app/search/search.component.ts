bc947cc1a1b86b282ce838f71002257d59dd168f
xniti3x <niti_3@hotmail.de>
2023-10-10 12:53:17 +0000

    component interaction

--- myapp/src/app/search/search.component.ts
+++ myapp/src/app/search/search.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { easepick } from '@easepick/bundle';
+import { DateTime } from '@easepick/datetime';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class SearchComponent {
 
+  public startDate: string = "";
+  public endDate: string = "";
+
+  public startDateFromChild($event: any): void {
+    this.startDate = $event;
+  }
+  public endDateFromChild($event: any): void {
+    this.endDate = $event;
+  }
 }
